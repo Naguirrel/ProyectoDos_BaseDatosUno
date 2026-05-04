f1b4483bea8f924267c621c7f982f4028536bd92
Norman <agu24479@uvg.edu.gt>
2026-05-03 18:47:56 -0600

    Add new reports

--- backend/src/controllers/reportes.controller.js
+++ backend/src/controllers/reportes.controller.js
@@ -37,4 +37,42 @@ const ingresosPorFecha = async (req, res) => {
     }
   };
 
-module.exports = { productosMasVendidos, ingresosPorFecha };
+const productosCaros = async (req, res) => {
+  try {
+    const result = await pool.query(`
+      SELECT nombre, precio_unitario
+      FROM producto
+      WHERE precio_unitario > (
+        SELECT AVG(precio_unitario) FROM producto
+      );
+    `);
+
+    res.json(result.rows);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Error en productos caros' });
+  }
+};
+
+const ventasCTE = async (req, res) => {
+  try {
+    const result = await pool.query(`
+      WITH ventas_totales AS (
+        SELECT id_producto, SUM(cantidad) AS total
+        FROM detalle_venta
+        GROUP BY id_producto
+      )
+      SELECT p.nombre, vt.total
+      FROM ventas_totales vt
+      JOIN producto p ON vt.id_producto = p.id_producto
+      ORDER BY vt.total DESC;
+    `);
+
+    res.json(result.rows);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Error en CTE de ventas' });
+  }
+};
+
+module.exports = { productosMasVendidos, ingresosPorFecha, productosCaros, ventasCTE };
--- backend/src/routes/reportes.routes.js
+++ backend/src/routes/reportes.routes.js
@@ -1,8 +1,23 @@
 const express = require('express');
 const router = express.Router();
 
-const { productosMasVendidos } = require('../controllers/reportes.controller');
+const {
+  ingresosPorFecha,
+  productosCaros,
+  ventasCTE,
+  productosMasVendidos
+} = require('../controllers/reportes.controller');
 
+// rutas
+router.get('/ingresos', ingresosPorFecha);
+router.get('/productos-caros', productosCaros);
+router.get('/ventas-cte', ventasCTE);
 router.get('/productos-mas-vendidos', productosMasVendidos);
 
+
+router.get('/', (req, res) => {
+    res.send('Reportes funcionando');
+  });
+
+// exportar
 module.exports = router;
